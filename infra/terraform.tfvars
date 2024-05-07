2d42684753542cf117752b9c00445ef7a79a09fe
ewertonnunes <ewerton.florencio@outlook.com>
2024-05-06 21:25:26 -0300

    Upload Documentation and Try ECR Configuration

--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -3,3 +3,5 @@ subnets            = ["subnet-02a847612875d595f", "subnet-094a63fc4a2245b06", "s
 internal           = false
 load_balancer_type = "application"
 vpc_id             = "vpc-0857ec9c2dc1874af"
+eks_cluster_name   = "app-eks-cluster"
+zone_id            = "Z02164713IB9NZTMDH6HU"
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,19 +1,34 @@
 variable "name" {
-  type = string
+  type        = string
+  description = "Nome base da Aplicação"
 }
 
 variable "subnets" {
-  type = list(string)
+  type        = list(string)
+  description = "Lista de Subnets"
 }
 
 variable "internal" {
-  type = bool
+  type        = bool
+  description = "Orientação de conectividade do Load Balancer Internal False or True indica conexão publica ou privada para internet."
 }
 
 variable "load_balancer_type" {
-  type = string
+  type        = string
+  description = "Tipo de Load Balancer Ex: Network, Application"
 }
 
 variable "vpc_id" {
-  type = string
+  type        = string
+  description = "ID de identificação da VPC"
+}
+
+variable "eks_cluster_name" {
+  type        = string
+  description = "Nome do Cluster EKS na AWS"
+}
+
+variable "zone_id" {
+  type        = string
+  description = "Nome da Zona de DNS do Route53"
 }
