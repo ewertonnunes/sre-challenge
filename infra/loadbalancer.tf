6b782a8a299bec6dfa74de32ae497b167144f62e
ewertonnunes <ewerton.florencio@outlook.com>
2024-05-07 22:35:36 -0300

    infra: create loadbalancer infra.

--- infra/loadbalancer.tf
+++ infra/loadbalancer.tf
@@ -7,3 +7,28 @@ resource "aws_lb" "app_load_balancer" {
 
   enable_deletion_protection = false
 }
+
+resource "aws_lb_target_group" "app_target_group" {
+  name        = var.name
+  target_type = "alb"
+  port        = 8080
+  protocol    = "TCP"
+  vpc_id      = var.vpc_id
+
+  health_check {
+    path     = "/"
+    port     = 8080
+    protocol = "HTTP"
+  }
+}
+
+resource "aws_lb_listener" "app_alb_listener" {
+  load_balancer_arn = aws_lb.app_load_balancer.arn
+  port              = 8080
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_target_group.arn
+  }
+}
